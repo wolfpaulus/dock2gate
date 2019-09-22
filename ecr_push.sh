fd0ee8317fa2c539e913288e8e26b1642500d708
Wolf Paulus <wolf@wolfpaulus.com>
2019-09-22 12:07:13 -0700

    updated variable names

--- ecr_push.sh
+++ ecr_push.sh
@@ -1,15 +1,20 @@
 #!/usr/bin/env bash
+#
+# build, tag and push a docker image into AWS ecr
+#
+AWS_PROFILE="edu"
+AWS_REGION="us-west-2"
 
-service_name=prime_ws
-tag_name=latest
-repo_name=178522735890.dkr.ecr.us-west-2.amazonaws.com
-img_name=tomcatprime
+SERVICE_NAME=prime_ws
+TAG_NAME=latest
+REPO_NAME=178522735890.dkr.ecr.us-west-2.amazonaws.com
+IMG_NAME=tomcatprime
 
-repo_uri=$repo_name/$service_name:$tag_name
+repo_uri=$REPO_NAME/$SERVICE_NAME:$TAG_NAME
 
 gradle clean
 gradle build
-docker build -t $img_name .
-docker tag $img_name:$tag_name $repo_uri
-eval $(aws ecr get-login --no-include-email --region us-west-2 --profile edu)
+docker build -t $IMG_NAME .
+docker tag $IMG_NAME:$TAG_NAME $repo_uri
+eval $(aws ecr get-login --no-include-email --region $AWS_REGION --profile $AWS_PROFILE)
 docker push $repo_uri
